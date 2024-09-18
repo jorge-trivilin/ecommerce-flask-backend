54cb6444a62cae68786f1ca5d5871b2a8341fe91
Jorge Trivilin <jorge.trivilin@gmail.com>
2024-09-18 21:48:26 +0000

    Auto-format code

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -77,7 +77,8 @@ def place_order():
     CartItem.query.filter_by(cart_id=cart.id).delete()
     db.session.commit()
 
-    return jsonify({"msg": "Order placed successfully", "order_id": order.id}), 201
+    return jsonify({"msg": "Order placed successfully",
+                   "order_id": order.id}), 201
 
 
 @orders_bp.route("/history", methods=["GET"])
@@ -93,7 +94,9 @@ def get_order_history():
         JSON response containing the user's order list.
     """
     user = User.query.get(get_jwt_identity())
-    orders = Order.query.filter_by(user_id=user.id).order_by(Order.id.desc()).all()
+    orders = Order.query.filter_by(
+        user_id=user.id).order_by(
+        Order.id.desc()).all()
 
     order_history = [
         {
